2c390d636c887186faf2ef0811cd6dc71cb9844b
CabBerumen <a19100031@ceti.mx>
2024-02-07 22:49:25 -0600

    Commit 1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       Perro perro = new Perro();
+       perro.ladrar();
+       perro.caminar();
+       
+       Pug pug = new Pug();
+       pug.Especie = "Perro";
+       pug.sufrir();
+       
+    }
+}
+
+public class Animal
+{
+    public string Especie;
+}
+
+public class Perro : Animal 
+{
+    public void ladrar()
+    {
+        Console.WriteLine("Perro ladra");
+    }
+    public void caminar()
+    {
+        Console.WriteLine("Perro camina");
+    }
+}
+
+public class Pug : Perro{
+    public void sufrir()
+    {
+        Console.WriteLine("Pug no sabe respirar");
+    }
+  
+}
+
